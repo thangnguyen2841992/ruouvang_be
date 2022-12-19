693d369a0acfeb709d4b982c2b3af9c21fb6821f
thangnguyen2841992 <ntthang4@cmcglobal.vn>
2022-12-19 17:01:31 +0700

    done

---
+++ Desktop/story/story/src/main/java/com/thang/story/model/entity/Type.java
@@ -0,0 +1,21 @@
+package com.thang.story.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="types")
+@Entity
+public class Type {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
