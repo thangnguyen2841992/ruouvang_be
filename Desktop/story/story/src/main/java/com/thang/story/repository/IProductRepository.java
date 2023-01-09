f8b55e6d2756ca3ebefd3c107a7904d022204ccc
thangnguyen2841990 <96616281+thangnguyen2841990@users.noreply.github.com>
2023-01-09 21:31:04 +0700

    d

--- Desktop/story/story/src/main/java/com/thang/story/repository/IProductRepository.java
+++ Desktop/story/story/src/main/java/com/thang/story/repository/IProductRepository.java
@@ -20,26 +20,26 @@ public interface IProductRepository extends JpaRepository<Product, Long> {
     @Query(value = "select * from products where accessory_id = 0", nativeQuery = true)
     List<Product> findAllAlcoholNoPagination();
     //Lấy danh sách phụ kiện rượu vang
-    @Query(value = "select * from products where accessory_id != 0  order by name limit 10 offset ?1", nativeQuery = true)
+    @Query(value = "select * from products where accessory_id != 0  order by name limit 12 offset ?1", nativeQuery = true)
     List<Product> findAllAccessory(int offset);
     //Lấy danh sách phụ kiện rượu vang khong phan trang
     @Query(value = "select * from products where accessory_id != 0", nativeQuery = true)
     List<Product> findAllAccessoryNoPagination();
 
     // Lay danh sach ruou vang chile
-    @Query(value = "select * from products where origin_id = ?1 and accessory_id = 0 order by name limit 10 offset ?2", nativeQuery = true)
+    @Query(value = "select * from products where origin_id = ?1 and accessory_id = 0 order by name limit 12 offset ?2", nativeQuery = true)
     List<Product> findProductsByOriginId(Long originId, int offset);
     // Lay danh sach ruou vang chile khong phan trang
     @Query(value = "select * from products where origin_id = ?1 and accessory_id = 0", nativeQuery = true)
     List<Product> findProductsByOriginIdNoPagination(Long originId);
     // Lay danh sach ruou vang trang
-    @Query(value = "select * from products where type_id = ?1 and accessory_id = 0 order by name limit 10 offset ?2", nativeQuery = true)
+    @Query(value = "select * from products where type_id = ?1 and accessory_id = 0 order by name limit 12 offset ?2", nativeQuery = true)
     List<Product> findProductsByTypeId(Long originId, int offset);
     // Lay danh sach ruou vang trang
     @Query(value = "select * from products where type_id = ?1 and accessory_id = 0", nativeQuery = true)
     List<Product> findProductsByTypeIdNoPagination(Long originId);
     //Lay danh sach cu the tung phu kien
-    @Query(value = "select * from products where accessory_id = ?1 and type_id = 0 and origin_id = 0  order by name limit 10 offset ?2", nativeQuery = true)
+    @Query(value = "select * from products where accessory_id = ?1 and type_id = 0 and origin_id = 0  order by name limit 12 offset ?2", nativeQuery = true)
     List<Product> findProductsByAccessoryId(Long accessoryId, int offset);
     //Lay danh sach cu the tung phu kien
     @Query(value = "select * from products where accessory_id = ?1 and type_id = 0 and origin_id = 0", nativeQuery = true)
